86cd67821ac0db993db3cf19dbf421c7759a7e47
Kim Gunsoo <gunsoo83.kim@samsung.com>
2015-05-04 01:18:29 +0000

    Add motor control code.

---
+++ src/in_rasp/motor_control.py
@@ -0,0 +1,68 @@
+import RPi.GPIO as gpio
+import time
+
+#Motor 1 Enable GPIO Pin
+IC12EN = 25
+
+#Motor 2 Enable GPIO Pin
+IC34EN = 18
+
+#Motor 1 GPIO Pin
+IC1A = 8
+IC2A = 7
+
+#Motor 2 GPIO Pin
+IC3A = 14
+IC4A = 15
+
+gpio.cleanup()
+
+gpio.setmode(gpio.BCM)
+
+#Pin Output Setup
+gpio.setup(IC12EN, gpio.OUT)
+gpio.setup(IC34EN, gpio.OUT)
+
+gpio.setup(IC1A, gpio.OUT)
+gpio.setup(IC2A, gpio.OUT)
+gpio.setup(IC3A, gpio.OUT)
+gpio.setup(IC4A, gpio.OUT)
+
+#Pin Initialization
+gpio.output(IC12EN, gpio.HIGH)
+gpio.output(IC34EN, gpio.HIGH)
+
+gpio.output(IC1A, gpio.LOW)
+gpio.output(IC2A, gpio.LOW)
+gpio.output(IC3A, gpio.LOW)
+gpio.output(IC4A, gpio.LOW)
+
+# RC BACK
+for i in range(100):
+    gpio.output(IC1A, gpio.HIGH)
+    time.sleep(0.01)
+
+gpio.output(IC1A, gpio.LOW)
+
+# RC FORWORD
+for i in range(100):
+    gpio.output(IC2A, gpio.HIGH)
+    time.sleep(0.01)
+
+gpio.output(IC2A, gpio.LOW)
+
+# LEFT
+for i in range(100):
+    gpio.output(IC3A, gpio.HIGH)
+    time.sleep(0.01)
+
+gpio.output(IC3A, gpio.LOW)
+
+# 
+for i in range(100):
+    gpio.output(IC4A, gpio.HIGH)
+    time.sleep(0.01)
+
+gpio.output(IC4A, gpio.LOW)
+
+gpio.cleanup()
---
+++ src/test/print.py
@@ -0,0 +1,7 @@
+import os,sys
+
+num1 = 10
+num2 = 20
+num3 = 30
+
+print "num1:%d, num2:%d, num3:%d" % (num1,num2,num3)
