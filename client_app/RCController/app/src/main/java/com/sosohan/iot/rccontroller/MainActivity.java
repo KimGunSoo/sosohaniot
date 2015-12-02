e22d4fffb294712fc7cc7e4b8f907de27c34cc66
Kim Gunsoo <gunsoo83.kim@samsung.com>
2015-12-02 20:09:00 +0900

    Refactoring RCController Android App.
    - Implements Init/Deinit/Go/Back feature that includes UI and rasp command routine.

--- client_app/RCController/app/src/main/java/com/sosohan/iot/rccontroller/MainActivity.java
+++ client_app/RCController/app/src/main/java/com/sosohan/iot/rccontroller/MainActivity.java
@@ -1,10 +1,7 @@
 package com.sosohan.iot.rccontroller;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v4.os.AsyncTaskCompat;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,30 +10,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.net.MalformedURLException;
+import java.net.HttpURLConnection;
 import java.net.URL;
-
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 public class MainActivity extends Activity{
 
-    //private static final String TAG = "MainActivity";
-    private static final String TAG = "gunsoo";
+    private static final String TAG = "MainActivity";
 
     private EditText ipEt;
     private EditText portEt;
     private EditText connectStateEt;
     private String ipString;
     private String portString;
-    private Button conBtn;
+    private Button initBtn;
+    private Button deinitBtn;
+    private Button goBtn;
+    private Button backBtn;
 
-    private static final String STR_CON_STATE_CONNECTED = "Complete connections.";
-    private static final String STR_CON_STATE_UNAVAILABLE_IP = "Unavailable internet address.";
-    private static final Integer CON_STATE_DISCONNECTED = 0;
-    private static final Integer CON_STATE_CONNECTED = 1;
-    private static final Integer CON_STATE_UNAVAILABLE_IP = 2;
-    private static Integer conState = CON_STATE_DISCONNECTED;
+    private static final Integer STATE_DEINITIALIZED = 0;
+    private static final Integer STATE_INITIALIZED = 1;
+    private static final Integer STATE_UNKNOWN = 2;
 
-    RaspConnectTask conTask;
+    private static final Integer CONTROL_ID_INIT = 0;
+    private static final Integer CONTROL_ID_DEINIT = 1;
+    private static final Integer CONTROL_ID_GO = 2;
+    private static final Integer CONTROL_ID_BACK = 3;
+    private static final Integer CONTROL_ID_GETSTATE = 4;
+
+    RaspControlTask raspTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,19 +49,31 @@ public class MainActivity extends Activity{
         ipEt = (EditText)findViewById(R.id.ip_editText);
         portEt = (EditText)findViewById(R.id.port_editText);
         connectStateEt = (EditText)findViewById(R.id.connect_state_editText);
-        conBtn = (Button)findViewById(R.id.connect_button);
+        initBtn = (Button)findViewById(R.id.init_button);
+        deinitBtn = (Button)findViewById(R.id.deinit_button);
+        goBtn = (Button)findViewById(R.id.go_button);
+        backBtn = (Button)findViewById(R.id.back_button);
+
+        goBtn.setEnabled(false);
+        backBtn.setEnabled(false);
+        deinitBtn.setEnabled(false);
+
+        RaspState.setRaspState(STATE_DEINITIALIZED);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "Enter onResume(), conState="+conState);
-        if (conState == CON_STATE_CONNECTED) {
-            connectStateEt.setText("connected");
-        } else {
-            connectStateEt.setText("disconnected");
-            conBtn.setEnabled(true);
-        }
+
+        //TODO: query the raspState to remote.
+        // 1.send getstate request
+        //raspTask = new RaspControlTask();
+        //raspTask.execute(new RaspCommand(CONTROL_ID_GETSTATE));
+        // 2. update RaspState
+        // RaspState.setRaspState(state);
+
+        Log.d(TAG, "Enter onResume(), raspState="+RaspState.getRaspState()+"("+RaspState.getRaspStateStr()+")");
+        updateUI();
     }
 
     @Override
@@ -83,56 +98,183 @@ public class MainActivity extends Activity{
         return super.onOptionsItemSelected(item);
     }
 
-    public void onClickedConnectBtn(View view) {
+    public void onClickedInitBtn(View view) {
         ipString = ipEt.getText().toString();
         portString = portEt.getText().toString();
-        conBtn.setEnabled(false);
 
-        Log.d(TAG, "connect to " + ipString + ":" + portString + "  [" + connectStateEt.getText().toString() + "]");
+        initBtn.setEnabled(false);
+        ipEt.setEnabled(false);
+        portEt.setEnabled(false);
+        connectStateEt.setText("initializing...");
 
-        connectStateEt.setText("connecting...");
-        conTask = new RaspConnectTask();
-        conTask.execute();
+        Log.d(TAG, "onClickedInitBtn");
+        Log.d(TAG, "rasp URL = " + ipString + ":" + portString);
+
+        /* send init request */
+        raspTask = new RaspControlTask();
+        raspTask.execute(new RaspCommand(CONTROL_ID_INIT));
     }
 
-    public void updateConnectStateUI() {
-        Log.d(TAG, "Enter onResume(), conState="+conState);
-        if (conState == CON_STATE_CONNECTED) {
-            connectStateEt.setText("connected");
+    public void onClickedDeinitBtn(View view) {
+        Log.d(TAG, "onClickedDeinitBtn");
+        /* send go request */
+        raspTask = new RaspControlTask();
+        raspTask.execute(new RaspCommand(CONTROL_ID_DEINIT));
+    }
+
+    public void onClickedGoBtn(View view) {
+        Log.d(TAG, "onClickedGoBtn");
+        /* send go request */
+        raspTask = new RaspControlTask();
+        raspTask.execute(new RaspCommand(CONTROL_ID_GO));
+    }
+
+    public void onClickedBackBtn(View view) {
+        Log.d(TAG, "onClickedBackBtn");
+        /* send back request */
+        raspTask = new RaspControlTask();
+        raspTask.execute(new RaspCommand(CONTROL_ID_BACK));
+    }
+
+    private void updateUI() {
+        connectStateEt.setText(RaspState.getRaspStateStr());
+        if (RaspState.getRaspState() == STATE_INITIALIZED) {
+            initBtn.setEnabled(false);
+            deinitBtn.setEnabled(true);
+            goBtn.setEnabled(true);
+            backBtn.setEnabled(true);
+            ipEt.setEnabled(false);
+            portEt.setEnabled(false);
         } else {
-            connectStateEt.setText("disconnected");
-            conBtn.setEnabled(true);
+            initBtn.setEnabled(true);
+            deinitBtn.setEnabled(false);
+            goBtn.setEnabled(false);
+            backBtn.setEnabled(false);
+            ipEt.setEnabled(true);
+            portEt.setEnabled(true);
         }
     }
 
-    public class RaspConnectTask extends AsyncTask<String, Integer, Integer> {
-        //private static final String TAG = "RaspConnectTask";
-        private static final String TAG = "gunsoo";
+    public class RaspControlTask extends AsyncTask<RaspCommand, Integer, Integer> {
+        private static final String TAG = "RaspControlTask";
+
+        @Override
+        protected Integer doInBackground(RaspCommand... params) {
+            RaspCommand cmd = params[0];
+            Log.d(TAG, "RaspControlTask : RaspCommand = " + cmd.getRaspCmd() +"(" +cmd.getRaspCmdStr()+")");
+
+            if (cmd.getRaspCmdStr() == null) {
+                Log.d(TAG, "Uknown controlID = " + cmd.getRaspCmd());
+                return 0;
+            }
+
+            Integer respCode = requestRestCmd(cmd.getRaspCmdStr());
+            publishProgress(cmd.getRaspCmd(), respCode);
+
+            return 0;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            Log.d(TAG, "onProgressUpdate controlID = " + values[0] +", response code = " + values[1]);
+            if (values[1] == 200) {
+                RaspState.setRaspState(STATE_INITIALIZED);
+                updateUI();
+            } else {
+                RaspState.setRaspState(STATE_DEINITIALIZED);
+                updateUI();
+            }
+        }
 
         @Override
         protected void onPostExecute(Integer integer) {
             super.onPostExecute(integer);
-            //Intent intent = new Intent(MainActivity.this, MainActivity.class);
-            //startActivity(intent);
-            updateConnectStateUI();
         }
 
-        @Override
-        protected Integer doInBackground(String... params) {
-            URL url = null;
-            String urlString = "http://" + ipString + ":" + portString;
-            Log.d(TAG, "Enter AsyncTask to connect remote server (" + urlString + ")");
+        private Integer requestRestCmd(String cmd) {
+            Log.d(TAG, "requestRestCmd : request = " + cmd);
+
+            Integer respCode = -1;
+            String restCmdUrlString = "http://" + ipString + ":" + portString + "/" +cmd;
+
+            Log.d(TAG, "restCmdUrlString = " + restCmdUrlString);
 
             try {
-                url = new URL(urlString);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-                conState = CON_STATE_UNAVAILABLE_IP;
-                return null;
-            }
+                URL url = new URL(restCmdUrlString);
+                HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
 
-            conState = CON_STATE_CONNECTED;
-            return null;
+                httpCon.setConnectTimeout(3000);
+                httpCon.setReadTimeout(3000);
+
+                httpCon.connect();
+
+                respCode = httpCon.getResponseCode();
+
+                InputStreamReader isr = new InputStreamReader(httpCon.getInputStream(), "UTF-8");
+                BufferedReader reader = new BufferedReader(isr);
+                StringBuilder builder = new StringBuilder();
+                String str;
+                while ((str = reader.readLine()) != null) {
+                    builder.append(str + "\n");
+                }
+                String resultStr = builder.toString();
+
+                Log.d(TAG, "requestRestCmd : http response code = " + respCode + ", resultStr = " + resultStr);
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+                return respCode;
+            }
+            return respCode;
+        }
+    }
+
+    public class RaspCommand {
+        private Integer raspCmd = -1;
+        private String raspCmdStr = null;
+
+        public RaspCommand(Integer cmd) {
+            raspCmd = cmd;
+            if (raspCmd == CONTROL_ID_INIT) {
+                raspCmdStr = "rasp_cmd_init";
+            } else if (raspCmd == CONTROL_ID_DEINIT) {
+                raspCmdStr = "rasp_cmd_deinit";
+            } else if (raspCmd == CONTROL_ID_GO) {
+                raspCmdStr = "rasp_cmd_go";
+            } else if (raspCmd == CONTROL_ID_BACK) {
+                raspCmdStr = "rasp_cmd_back";
+            }
+        }
+        public Integer getRaspCmd() {
+            return raspCmd;
+        }
+        public String getRaspCmdStr() {
+            return raspCmdStr;
+        }
+    }
+
+    public static class RaspState {
+        private static Integer raspState = -1;
+        private static String raspStateStr = null;
+
+        private static final String STATE_DEINITIALIZED_STR = "Deinitialized";
+        private static final String STATE_INITIALIZED_STR = "Initialized";
+        private static final String STATE_UNKNOWN_STR = "Unknown State";
+
+        public static void setRaspState(Integer state) {
+            raspState = state;
+            if (raspState == STATE_DEINITIALIZED) {
+                raspStateStr = STATE_DEINITIALIZED_STR;
+            } else if (raspState == STATE_INITIALIZED) {
+                raspStateStr = STATE_INITIALIZED_STR;
+            } else if (raspState == STATE_UNKNOWN) {
+                raspStateStr = STATE_UNKNOWN_STR;
+            }
+        }
+        public static Integer getRaspState() {
+            return raspState;
+        }
+        public static String getRaspStateStr() {
+            return raspStateStr;
         }
     }
 }
